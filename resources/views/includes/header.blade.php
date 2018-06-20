9a0ff2944a20fb6c1c251a01dc10d2e077d0e50e
Emmanuel See Te <bsit.emmanuel@gmail.com>
2018-06-20 20:14:02 +0800

    changelog-20180620

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,32 +1,33 @@
-
 <div class="wrapper row1">
-<div id="logo_ptc">
-<img src="{{ asset('images/logo_ptc.png') }}" alt="PTC-LOGO">
-</div>
-
-  <header id="header" class="hoc clear"> 
-  
-  <div id="logo" class="fl_left">
-   
-      <h1 style="color:#065608"><b>Pateros Technological College</b></h1>
-      <p style="color:#a39c01">College St., Sto. Rosario-Kanluran Pateros, Metro Manila, 1620<br>
-            642-8896 . 640-5389</p>
-      <p style="color:#017c17">Broadening Academic Horizons: 2018 and Beyond</p>
+    <div id="logo_ptc">
+        <img src="{{ asset('images/logo_ptc.png') }}" alt="PTC-LOGO">
     </div>
 
-    <div class="fl_right">
-      <ul class="faico clear">
-        <div style="font-size:3em">
-        <li><a class="faicon-facebook" href="#"><i class="fab fa-facebook-square" style="color:#3B5998"></i></a></li>
-        <li><a class="faicon-twitter" href="#"><i class="fab fa-twitter" style="color: #00ACEE"></i></a></li>
-        <li><a class="faicon-youtube" href="#"><i class="fab fa-youtube-square" style="color:#C8232C"></i></a></li>
-        <li><a class="faicon-linkedin" href="#"><i class="fab fa-linkedin" style="color:#0E76A8"></i></a></li>
-        <li><a class="faicon-google-plus" href="#"><i class="fab fa-google-plus-square" style="color:#DB4A39"></i></a></li>
-        <li><a class="faicon-instagram" href="#"><i class="fab fa-instagram" style="color:#f09433"></i></a></li>
+    <header id="header" class="hoc clear"> 
+
+        <div id="logo" class="fl_left">
+
+        <h1 style="color:#065608" class="font-weight-light">{{ config('app.name', 'PTC-Official') }}</h1>
+        <p style="" class="font-weight-light font-italic">
+            <span style="color:#a39c01">
+                College St., Sto. Rosario-Kanluran Pateros, Metro Manila, 1620<br>
+                642-8896 / 640-5389<br>
+            </span>
+            <span style="color:#017c17">
+                Broadening Academic Horizons: 2018 and Beyond
+            </span>
+        </p>
         </div>
-      </ul>
-    </div>
 
-  
+        <div class="fl_right">
+            <ul class="faico clear">
+                <li><a class="faicon-facebook" href="#"><i class="fab fa-facebook-square" style="color:#3B5998"></i></a></li>
+                <li><a class="faicon-twitter" href="#"><i class="fab fa-twitter" style="color: #00ACEE"></i></a></li>
+                <li><a class="faicon-youtube" href="#"><i class="fab fa-youtube-square" style="color:#C8232C"></i></a></li>
+                <li><a class="faicon-linkedin" href="#"><i class="fab fa-linkedin" style="color:#0E76A8"></i></a></li>
+                <li><a class="faicon-google-plus" href="#"><i class="fab fa-google-plus-square" style="color:#DB4A39"></i></a></li>
+                <li><a class="faicon-instagram" href="#"><i class="fab fa-instagram" style="color:#f09433"></i></a></li>
+            </ul>
+        </div>
     </header>
 </div>
--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,112 +1,110 @@
+<nav id="navbar" class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark d-block ptc-navbar">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse p-2" id="navbarContent">
+        <ul class="navbar-nav">
+            <!-- administration -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
+                        Administration <span class="caret"></span>
+                    </a>
 
-<nav id="navbar" class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark d-block ">
-       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-           <span class="navbar-toggler-icon"></span>
-       </button>
-
-       <div class="collapse navbar-collapse p-2" id="navbarContent">
-       <ul class="navbar-nav">
-           <!-- administration -->
-               <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
-                       Administration <span class="caret"></span>
-                   </a>
-
-                   <div class="dropdown-menu rounded-10" >
-                       
-                       <a class="dropdown-item" href="">Office of the President</a>
-                       <a class="dropdown-item" href="">Office of the Vice President for Administration and Finance</a>
-                       <a class="dropdown-item" href="#">Board of Trustees</a>
+                    <div class="dropdown-menu rounded-10" >
+                        
+                        <a class="dropdown-item" href="">Office of the President</a>
+                        <a class="dropdown-item" href="">Office of the Vice President for Administration and Finance</a>
+                        <a class="dropdown-item" href="#">Board of Trustees</a>
                         <a class="dropdown-item" href="#">Management Committee (Mancom)</a>
                         <a class="dropdown-item" href="#">College Department Faculty</a>
                         <a class="dropdown-item" href="#">Senior High School Department Faculty</a>
                         <a class="dropdown-item" href="#">ITRED Faculty and Staff</a>
                         <a class="dropdown-item" href="#">Administrative Personnel</a>  
-                   </div>
-               </li>
+                    </div>
+                </li>
             <!-- organization -->
-               <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
-                       Organization <span class="caret"></span>
-                   </a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
+                        Organization <span class="caret"></span>
+                    </a>
 
-                   <div class="dropdown-menu rounded-10" >
-                       
-                   <a class="dropdown-item" href="#">Faculty Club</a>
+                    <div class="dropdown-menu rounded-10" >
+                        
+                    <a class="dropdown-item" href="#">Faculty Club</a>
                     <a class="dropdown-item" href="#">PTC Cup</a>
                     <a class="dropdown-item" href="#">College Supreme Student Council</a>
                     <a class="dropdown-item" href="#">PTC Student Volunteers</a>
                     <a class="dropdown-item" href="#">Senior High School Officers</a>    
-                   </div>
-               </li>
+                    </div>
+                </li>
 
-             <!-- academics -->
-             <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
-                       Academics <span class="caret"></span>
-                   </a>
+                <!-- academics -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
+                        Academics <span class="caret"></span>
+                    </a>
 
-                   <div class="dropdown-menu rounded-10" >
-                  <div class="nav-item dropdown">     
-                   <a class="dropdown-item" href="#">Four Year Program</a>
+                    <div class="dropdown-menu rounded-10" >
+                    <div class="nav-item dropdown">     
+                    <a class="dropdown-item" href="#">Four Year Program</a>
                     <a class="dropdown-item" href="#">Two Year Program</a>
                     <a class="dropdown-item" href="#">Senior High School (SHS)</a>
                     <a class="dropdown-item" href="#">Executive Program</a>
                 </div>     
-                   </div>
-               </li>  
+                    </div>
+                </li>  
 
-             <!-- admission -->
-             <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
-                       Admission <span class="caret"></span>
-                   </a>
+                <!-- admission -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
+                        Admission <span class="caret"></span>
+                    </a>
 
-                   <div class="dropdown-menu rounded-10" >
-                  <div class="nav-item dropdown">     
-                   <a class="dropdown-item" href="#">College Application</a>
-                   <a class="dropdown-item" href="#">Senior High School Application (SHS)</a>
+                    <div class="dropdown-menu rounded-10" >
+                    <div class="nav-item dropdown">     
+                    <a class="dropdown-item" href="#">College Application</a>
+                    <a class="dropdown-item" href="#">Senior High School Application (SHS)</a>
                 </div>     
-                   </div>
-               </li>  
- 
-             <!-- student -->
-             <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
-                       Student <span class="caret"></span>
-                   </a>
+                    </div>
+                </li>  
 
-                   <div class="dropdown-menu rounded-10" >
-                  <div class="nav-item dropdown">     
-                   <a class="dropdown-item" href="#">Student</a>
+                <!-- student -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
+                        Student <span class="caret"></span>
+                    </a>
 
-                </div>     
-                   </div>
-               </li>                 
-
-             <!-- alumni -->
-             <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
-                       Alumni <span class="caret"></span>
-                   </a>
-
-                   <div class="dropdown-menu rounded-10" >
-                  <div class="nav-item dropdown">     
-                   <a class="dropdown-item" href="#">Alumni</a>
+                    <div class="dropdown-menu rounded-10" >
+                    <div class="nav-item dropdown">     
+                    <a class="dropdown-item" href="#">Student</a>
 
                 </div>     
-                   </div>
-               </li>  
- 
-             <!-- about ptc -->
-             <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
-                       About <span class="caret"></span>
-                   </a>
+                    </div>
+                </li>                 
 
-                   <div class="dropdown-menu rounded-10" >
-                  <div class="nav-item dropdown">     
+                <!-- alumni -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
+                        Alumni <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu rounded-10" >
+                    <div class="nav-item dropdown">     
+                    <a class="dropdown-item" href="#">Alumni</a>
+
+                </div>     
+                    </div>
+                </li>  
+
+                <!-- about ptc -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
+                        About <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu rounded-10" >
+                    <div class="nav-item dropdown">     
                     <a class="dropdown-item" href="#">HISTORY</a>
                     <a class="dropdown-item" href="#">Mission/Core Values</a>
                     <a class="dropdown-item" href="#">PTC Hymn</a>
@@ -115,26 +113,17 @@
                     <a class="dropdown-item" href="#">PTC Under the New Administration</a>
 
                 </div>     
-                   </div>
-               </li> 
-               
-               
-               
+                    </div>
+                </li> 
+                
+                
+                
 
             
-               
+                
 
 
-         </li>
+            </li>
         </ul>
-           
-           
-       </div>
-
-
-
+    </div>
 </nav>
-
-
-
-
