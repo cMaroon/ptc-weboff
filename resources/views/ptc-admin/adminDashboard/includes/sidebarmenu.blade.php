03a7ac751624fb4fbfda5de85571846890d49760
cMaroon <nworld.kazz@gmail.com>
2018-06-27 18:30:13 +0800

    cmaroon-changelog-20180627

---
+++ resources/views/ptc-admin/adminDashboard/includes/sidebarmenu.blade.php
@@ -0,0 +1,31 @@
+<!-- menu profile quick info -->
+<div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="{{ asset('images/logo_ptcbw.png') }}" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>Profile Name Here</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="index.html">Add Employee</a></li>
+                      <li><a href="index2.html">Add Student</a></li>
+                      <li><a href="index3.html">View Report</a></li>
+                    </ul>
+                  </li>
+                 
+              </div>
+
+            </div>
+            <!-- /sidebar menu -->
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,11 @@
 // Route::get('/', 'PagesController@landing')->name('landing');
 Route::get('/', 'PagesController@home')->name('home');
 Route::get('/ptc-admin/adminDashboard/dashboard', 'PagesController@dashboard')->name('dashboard');
+
+
+/*
+|--------------------------------------------------------------------------
+| Posts Routes
+|--------------------------------------------------------------------------
+*/
+Route::resource('posts','PostsController');
