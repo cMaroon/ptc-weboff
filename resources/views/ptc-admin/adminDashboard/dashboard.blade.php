1c83ff743125758bf40e6b34481262bf0554a26e
cMaroon <nworld.kazz@gmail.com>
2018-07-10 19:58:35 +0800

    cmaroon-changelog-20180710

--- resources/views/ptc-admin/adminDashboard/dashboard.blade.php
+++ resources/views/ptc-admin/adminDashboard/dashboard.blade.php
@@ -10,25 +10,7 @@
              @include('ptc-admin.adminDashboard.includes.sidebarmenu')
 
             <!-- /menu footer buttons -->
-            <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Settings">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="FullScreen" onclick="openFullscreen();">
-                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true" ></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Lock">
-                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="{{ route('logout') }}"  onclick="event.preventDefault();
-              document.getElementById('logout-form').submit();">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-                
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  @csrf
-              </form>
-              </a>
-            </div>
+            @include('ptc-admin.adminDashboard.includes.menufooter')
             <!-- /menu footer buttons -->
           </div>
         </div>
